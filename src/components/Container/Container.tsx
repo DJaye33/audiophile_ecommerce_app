1d7a5fd9c1bbf291e2dedac1e434c77404763209
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-20 23:53:50 -0400

    add navbar divider and responsiveness

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,9 +1,11 @@
+import styles from "./Container.module.css";
+
 interface ContainerProps {
   children?: React.ReactNode;
 }
 
 const Container = ({ children }: ContainerProps): JSX.Element => (
-  <div>{children}</div>
+  <div className={styles.container}>{children}</div>
 );
 
 export default Container;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import Container from "../Container/Container";
 
+import styles from "./NavBar.module.css";
+
 import menuBtn from "../../assets/shared/tablet/icon-hamburger.svg";
 import brandLogo from "../../assets/shared/desktop/logo.svg";
 import cartIcon from "../../assets/shared/desktop/icon-cart.svg";
 
 const NavBar = () => {
   return (
-    <div>
+    <div className={styles.navBarContainer}>
       <Container>
-        <nav>
-          <button>
+        <nav className={styles.nav}>
+          <button className={styles.navBtn}>
             <img src={menuBtn} alt="menu" />
           </button>
           {/* {TODO: Replace a tags with router dom Link} */}
           <a href="/">
             <img src={brandLogo} alt="audiophile logo" />
           </a>
-          <button>
+          <button className={styles.navBtn}>
             <img src={cartIcon} alt="shopping cart" />
           </button>
         </nav>
       </Container>
+      <hr className={styles.divider} />
     </div>
   );
 };
